8c9f14ee1b02c04920a53340ab8df8910f8ed926
Ammar Ashour <am9370337@gmail.com>
2024-08-06 00:47:38 +0300

    add readme

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-    let task = false;
+    let task = false; // let because its block scoped, unlike var which is function scooped
     let task2 = true;
   
     if (trueOrFalse) {
-      let task = true;
+      let task = true; 
       let task2 = false;
     }
   
